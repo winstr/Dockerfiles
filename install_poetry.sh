4528381009007575b227637cb90789e3f6a4853e
winstr <seunghyeon.me@gmail.com>
2024-01-12 10:59:45 +0900

    updated.

--- install_poetry.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-POETRY_HOME=/root/.poetry
-PATH=$POETRY_HOME/bin:$PATH
-curl -sSL https://install.python-poetry.org | POETRY_HOME=$POETRY_HOME python3 -
-poetry config virtualenvs.in-project true
---
+++ ubuntu/tensorflow-devel/run_container.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+IMGNAME=ubuntu-tensorflow-devel
+DSTREPO=winstr/linux:$IMGNAME
+WORKDIR=$HOME/workspace/docker/containers
+
+docker run -it --gpus all --network host \
+    -v $WORKDIR/$IMGNAME:/workspace \
+    --name $IMGNAME $DSTREPO \
+    /bin/bash
+
